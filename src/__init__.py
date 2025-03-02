3d56143c59d48b125d573e042bbb7003f2793114
Meenal Sharma <119120576+MeenalMSharma@users.noreply.github.com>
2025-03-02 20:44:18 +0530

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+from .prediction_page import prediction_page
+from .about import about
+from .mail import mail
+from .home import home
+from .pneumonia import pneumonia
+from .blood_cancer import blood_cancer
